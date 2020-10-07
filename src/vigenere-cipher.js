db06e31049dc1ac7c34dae90621fbbb4cde56ab4
DmitryBogdan90 <mgblsttt@gmail.com>
2020-10-07 20:17:54 +0300

    feat: add Vigenere Cipher solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,68 @@
-const CustomError = require("../extensions/custom-error");
-
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(typeMachine = true) {
+    this.typeMachine = typeMachine;
+  }
+
+  encrypt(message, key) {
+    if(!message || !key) {
+      throw 'THROWN';
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let i = 0;
+    let result = [];
+    let count = 0;
+    while (key.length < message.length) {
+      key += key;
+    }
+    while (i < message.length) {
+      let m = message[i].charCodeAt(0) - 65;
+      let k = key[count].charCodeAt(0) - 65;
+      if (m >= 0 && m <= 25) {
+        let c = (m + k) % 26 + 65;
+        result.push(String.fromCharCode(c));
+      } else {
+        result.push(message[i]);
+        count--;
+      }
+      count++;
+      i++;
+    }
+    if (!this.typeMachine) {
+      return result.reverse().join('');
+    }
+    return result.join('');
+  }
+
+  decrypt(cryptoWord, key) {
+    if(!cryptoWord || !key) {
+      throw 'THROWN';
+    }
+    cryptoWord = cryptoWord.toUpperCase();
+    key = key.toUpperCase();
+    let i = 0;
+    let result = [];
+    let count = 0;
+    while (key.length < cryptoWord.length) {
+      key += key;
+    }
+    while (i < cryptoWord.length) {
+      let c = cryptoWord[i].charCodeAt(0) - 65;
+      let k = key[count].charCodeAt(0) - 65;
+      if (c >= 0 && c <= 25) {
+        let m = (c + 26 - k) % 26 + 65;
+        result.push(String.fromCharCode(m));
+      } else {
+        result.push(cryptoWord[i]);
+        count--;
+      }
+      count++;
+      i++;
+    }
+    if (!this.typeMachine) {
+      return result.reverse().join('');
+    }
+    return result.join('');
   }
 }
 
